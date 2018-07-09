8abf48334f1c00d6b5da0c4f9c78b0a258b97e0b
Laurel Herting <laurel.herting@gmail.com>
2018-07-09 16:15:24 -0600

    edit port

--- http_server.js
+++ http_server.js
@@ -1,11 +1,16 @@
 const http = require('http');
+const minimist = require('minimist');
+
+const argv = minimist(process.argv.slice(2));
+const port = argv.port;
 
 function http_handler(req, res) {
   res.writeHead(200, {'Content-type': 'text/plain'});
-  res.end('Hello World \n');
+  res.end('Hello World \nI am running at ${port}');
 }
 
 http.createServer(http_handler)
-  .listen(2222, '127.0.0.1');
+  .listen(port, '127.0.0.1');
 
-console.log('HTTP Server running at http://127.0.0.1.2222');
+console.log('HTTP Server running at http://127.0.0.1:{port}');
+
