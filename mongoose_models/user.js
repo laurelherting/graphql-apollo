c86609a92474ddf1d192876c9da5afd5cff920bc
Laurel Herting <laurel.herting@gmail.com>
2018-07-09 17:10:13 -0600

    create mongo schema for user

---
+++ mongoose_models/user.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: { type: String }
+});
+
+const User = mongoose.model('user', UserSchema);
+module.exports = User;
