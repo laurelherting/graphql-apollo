5aaaae157f7f4944066e8caf1d6623e3b7dfa299
Laurel Herting <laurel.herting@gmail.com>
2018-07-10 10:03:05 -0600

    add data to promise return

--- mongoose_save_instance.js
+++ mongoose_save_instance.js
@@ -1,5 +1,14 @@
 const db = require('./mongoDB_connection');
 
-const User = require('./');
+const User = require('./mongoose_models/user');
+const player = new User({
+  name: "player"
+});
 
+player.save()
+  .then( (user) => {
+    console.log(user);
+  });
+
+db.connection.close();
 
