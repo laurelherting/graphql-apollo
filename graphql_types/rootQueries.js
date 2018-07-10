539eda21740f6c366e3289c9f118a913ca4d71fe
Laurel Herting <laurel.herting@gmail.com>
2018-07-10 16:21:28 -0600

    wip: assemble schema

---
+++ graphql_types/rootQueries.js
@@ -0,0 +1,16 @@
+const graphql = require('graphql');
+const { GraphQLObjectType, GraphQLList, GraphQLID, GraphQLString } = graphql;
+
+const UserType = require('./user_type');
+const UserModel = require('../mongoose_models/user');
+
+const RootQueryType = new GraphQLObjectType ({
+  name: 'RootQuery',
+  fields: () => ({
+    users: {
+      type: new GraphQLList(UserType)
+    }
+  })
+})
+
+
