2c18e4daaca70715a089efd58bfe7abca4181301
Laurel Herting <laurel.herting@gmail.com>
2018-07-11 10:31:02 -0600

    implement CORS for security

---
+++ express_graphql_server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const expressGraphQL = require('express-graphql');
+const schema = require('./graphql_types/graphql_schema');
+const db = require('./mongoDB_connection');
+
+const app = express();
+
+app.use(cors());
+
+app.use('/graphql', expressGraphQL ({
+  schema: schema,
+  graphql: true
+}));
+
+app.listen(4000, () => {
+  console.log('Running a CORS-enabled GraphQL API server at localhost:4000/graphql'),
+});
+
