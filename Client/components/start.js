b2aef85f886dd3decef172b4216d1d1bd539eeb5
Laurel Herting <laurel.herting@gmail.com>
2018-07-13 13:21:23 -0600

    make Start component dynamic, refactoring

--- Client/components/start.js
+++ Client/components/start.js
@@ -1,12 +1,29 @@
 const React = require('react');
+const { graphql, createFragmentContainer } = require('react-relay');
 
 class Start extends React.Component {
   render () {
+    const { user } =  this.props.userToGreet;
+
     return (
-      <h1>Hello, {this.props.name}</h1>
+      <div>
+        <h4>Greeting Component</h4>
+        <p>Hello, user {user.name}</p>
+        <p>Your ID is: {user.id}</p>
+      </div>
     );
   }
 }
 
-module.exports = Start;
+module.exports = createFragmentContainer(
+  Start,
+  graphql`
+  fragment Start_userToGreet on Viewer {
+    user {
+      name
+      id
+    }
+  }
+  `
+);
 
