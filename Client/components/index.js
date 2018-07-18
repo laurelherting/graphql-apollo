616f1aed8ab17efef6436fcdda8b79e472f4bacc
Laurel Herting <laurel.herting@gmail.com>
2018-07-18 17:02:46 -0600

    wip-QueryRenderer

--- Client/components/index.js
+++ Client/components/index.js
@@ -1,22 +1,30 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
-
-import {
-  QueryRenderer,
-  graphql
-} from 'react-relay';
+import { QueryRenderer, graphql } from 'react-relay';
 
 import environment from './environment';
 import App from './App';
 
-const RootComponent = () => {
+class RendererHelper extends React.Component {
+  render() {
+    return (
+      <QueryRenderer
+        environment={environment}
+        query={graphql`
+          query ChangeName() {
+            name
+          }
+        `}
+        render={({error, props}) => {
+          if (error) {
+            return <div>{error.message}</div>;
+          } else if (props) {
+            return <div>{props.page.name} works!</div>;
+          }
+          return <div>Loading</div>;
+        }
+      />
+    );
+  }
+}
 
- return;
-
-};
-
-ReactDOM.render(
-  <RootComponent />,
-  document.getElementById('root')
-);
-
+module.exports = RenderHelper;
