b7c50f9e112599a487f6fc1dbdfd9d4fd7541643
Laurel Herting <laurel.herting@gmail.com>
2018-07-18 10:50:12 -0600

    add QueryRenderer

--- Client/components/index.js
+++ Client/components/index.js
@@ -1,8 +1,9 @@
 const ReactDOM = require('react-dom');
 const React = require('react');
+const { QueryRenderer, graphql } = require('react-relay');
+const environment = require('./environment');
 
 const App = require('./App');
-
 
 function RootComponent() {
   const rootQuery = graphql`
@@ -13,7 +14,17 @@ function RootComponent() {
     }
   `;
 
-  function onRenderHelper();
+  function onRenderHelper({ err, props }) {
+    if (err) {
+      return <div>{err.message}</div>
+    } else if (props) {
+        return <App appUser={props.random_label2} />
+    } else {
+      return <div>Loading...</div>
+    }
+  }
+
+  return <QueryRenderer environment={environment} query={rootQuery} render={onRenderHelper} />;
 }
 
 ReactDOM.render(
