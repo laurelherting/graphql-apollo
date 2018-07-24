56d656c00f7360ffe563ca46c9a327a18be518bb
Laurel Herting <laurel.herting@gmail.com>
2018-07-24 16:21:36 -0600

    wip-change user name by id

--- graphql_types/mutations.js
+++ graphql_types/mutations.js
@@ -7,18 +7,20 @@ const UserType = require('./user_type.js');
 
 const mutations = new GraphQLObjectType({
   name: 'Mutations',
+  description: 'These are things we can change',
   fields: () => ({
     changeUserName: {
       type: UserType,
+      description: 'Change a user with id and return the newName.',
       args: {
         newName: { type: GraphQLString },
         userId: { type: GraphQLID }
       },
-      resolve(parentValue, { newName, userId }) {
+      resolve: (parentValue, { newName, userId }) => {
         return User.changeName(userId, newName);
       }
     }
-  })
+  }),
 });
 
 module.exports = mutations;
