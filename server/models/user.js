0ff75ae4def39bb9b8a3cac497d085cb4d2cafdb
Laurel Herting <laurel.herting@gmail.com>
2018-08-15 15:42:26 -0600

    change to BookSchema

--- server/models/user.js
+++ server/models/user.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
+const BookSchema = new Schema({
   name: String
 });
 
-UserSchema.statistics = function(userID, newName) {
-  const User = mongoose.model('user');
+BookSchema.statistics = function(bookID, newName) {
+  const Book = mongoose.model('book');
 
-  return User.findById(userId)
-    .then(user => {
-    user.name = newName;
-    return user.save();
+  return Book.findById(bookId)
+    .then(book => {
+    book.name = newName;
+    return book.save();
     })
 }
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('Book', BookSchema);
