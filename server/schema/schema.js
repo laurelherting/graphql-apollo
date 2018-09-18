47af8d4267364dd0df5aa4ceca940a1559fa4769
Laurel Herting <laurel.herting@gmail.com>
2018-09-18 17:00:43 -0600

    add new mutation: addBook

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -94,6 +94,14 @@ const Mutation = new GraphQLObjectType({
         return author.save();
       }
     }
+  },
+  addBook: {
+    type: BookType,
+    args: {
+      name: { type: GraphQLString },
+      genre: { type: GraphQLString },
+      authorId: { type: GraphQLID }
+    },
   }
 });
 
