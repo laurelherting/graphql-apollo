aaa41b10deaf5e44818bb0795e0c1ef8c279e78d
Laurel Herting <laurel.herting@gmail.com>
2018-09-21 10:13:32 -0600

    add BookList

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import BookList from './components/BookList';
+
 class App extends Component {
   render() {
     return (
       <div id="Main">
         <h1>My Reading List</h1>
+        <BookList />
       </div>
     );
   }
