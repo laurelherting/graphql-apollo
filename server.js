d3f6953d5e187297ab840f9fd2dc412cd6cd0160
Laurel Herting <laurel.herting@gmail.com>
2018-09-29 18:29:56 -0600

    wip-change order for cors, add comments, update port

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 const express = require('express');
-const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./server/schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
-const port = 4000;
 
+// allow cross-origin requests
+app.use(cors());
+
+// connect to mlab
 mongoose.connect('mongodb://author:12ddyy94@ds231991.mlab.com:31991/relay_work');
 mongoose.connection.once('open', () => {
     console.log('connected to database');
 });
 
-app.use(cors());
-
+// bind express with graphql
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }));
 
-app.listen(port, () =>
-	console.log(`GraphQL Server running at http://127.0.0.1:${port}`)
-);
+app.listen(4000, () => {
+  console.log('now listening for requests on port 4000');
+});
