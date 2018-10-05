9da2b9921ca471b9dcd114cdf37a906db3e417ab
Laurel Herting <laurel.herting@gmail.com>
2018-10-04 21:57:46 -0600

    update for query file

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,16 @@ const client = new ApolloClient({
 });
 
 class App extends Component {
+  displayAuthors() {
+    let data = this.props.data;
+        if (data.loading) {
+            return( <option disabled>Loading authors</option> );
+        } else {
+            return data.authors.map(author => {
+                return( <option key={ author.id } value={author.id}>{ author.name }</option> );
+            });
+        }
+    }
   render() {
     return (
       <ApolloProvider client={client}>
