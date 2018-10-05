3943c39999bab902ac41b412486f485e9dcadac3
Laurel Herting <laurel.herting@gmail.com>
2018-10-05 14:40:38 -0600

    update component state-attach event listener

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -26,21 +26,25 @@ class AddBook extends Component {
         });
       }
     }
+    submitForm(e) {
+      e.preventDefault()
+      console.log(this.state);
+    }
 
     render() {
       return (
-        <form id="add-book">
+        <form id="add-book" onSubmit={ this.submitForm.bind(this) }>
           <div className="field">
             <label>Book name:</label>
-            <input type="text" />
+            <input type="text" onChange={ (e) => this.setState({ name: e.target.value }) }/>
           </div>
           <div className="field">
             <label>Genre:</label>
-            <input type="text" />
+          <input type="text" onChange={ (e) => this.setState({ genre: e.target.value }) } />
           </div>
           <div className="field">
             <label>Author:</label>
-            <select>
+            <select onChange={ (e) => this.setState({ authorId: e.target.value }) } >
               <option>Select author</option>
               {this.displayAuthors()}
             </select>
