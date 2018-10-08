0c35c20f1b5c08f535479a0c9b38f1884bd11070
Laurel Herting <laurel.herting@gmail.com>
2018-10-08 17:09:21 -0600

    compose queries

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -12,8 +12,8 @@ class AddBook extends Component {
     };
   }
   displayAuthors() {
-    let data = this.props.data;
-
+    console.log(this.props);
+    let data = this.props.getAuthorsQuery;
     if (data.loading) {
       return (
         <option disabled>Loading authors</option>
@@ -29,6 +29,7 @@ class AddBook extends Component {
     submitForm(e) {
       e.preventDefault()
       console.log(this.state);
+      this.props.addBookMutation(); // adds book, but with no values
     }
 
     render() {
@@ -55,4 +56,7 @@ class AddBook extends Component {
     }
 }
 
-export default graphql(getAuthorsQuery)(AddBook);
+export default compose(
+  graphql(getAuthorsQuery, { name: "getAuthorsQuery" }),
+  graphql(addBookMutation, { name: "addBookMutation" })
+)(AddBook);
