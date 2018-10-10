b81da9220b61fc6c64b2733f917752c830b8ddd9
Laurel Herting <laurel.herting@gmail.com>
2018-10-09 18:44:18 -0600

    access variables in mutation

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -19,8 +19,9 @@ const getBooksQuery = gql`
 `;
 
 const addBookMutation = gql`
-  mutation {
-    addBook(name: "", genre: "", authorId: "") {
+  mutation addBook(
+    $name: String!, $genre: String!, $authorId: ID!) {
+      addBook(name: $name, genre: $genre, authorId: $authorId) {
       name
       id
     }
