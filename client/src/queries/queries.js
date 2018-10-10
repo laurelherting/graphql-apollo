b2c7c973058a34052db079891e40e31609c30371
Laurel Herting <laurel.herting@gmail.com>
2018-10-10 16:06:18 -0600

    remove name

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -19,7 +19,7 @@ const getBooksQuery = gql`
 `;
 
 const addBookMutation = gql`
-  mutation addBook($name: String!, $genre: String!, $authorId: ID!) {
+  mutation($name: String!, $genre: String!, $authorId: ID!) {
     addBook(name: $name, genre: $genre, authorId: $authorId) {
       name
       id
