c421ded3b783b4012a6b9ea2d99c7ebbcd98224e
Laurel Herting <laurel.herting@gmail.com>
2018-10-12 17:12:08 -0600

    wip-query

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -3,6 +3,13 @@ import { graphql } from 'react-apollo';
 import { getBookQuery } from '../queries/queries';
 
 class BookDetails extends Component {
+  displayBookDetails() {
+    const { book } = this.props.data;
+    if () {
+      return ()
+    }
+  }
+
   render() {
     return(
       <div id="book-details">
