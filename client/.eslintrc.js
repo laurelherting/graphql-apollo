a8e026c1f2e7b195652ffb5813217f411da58012
Laurel Herting <laurel.herting@gmail.com>
2018-10-17 13:31:03 -0600

    add eslint configuration

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,12 +1,57 @@
 module.exports = {
-    "plugins": [
-        "prettier"
-    ],
+ "lint-staged": {
+    "*.js": [
+      "eslint --fix",
+      "git add"
+    ]
+  },
+  "eslintConfig": {
     "extends": [
-        "airbnb-base",
-        "prettier"
+      "airbnb",
+      "prettier",
+      "prettier/react"
+    ],
+    "env": {
+      "browser": true
+    },
+    "parserOptions": {
+      "ecmaVersion": 6
+    },
+    "plugins": [
+      "prettier"
     ],
     "rules": {
-        "prettier/prettier": ["error", { "singleQuote": true, "trailingComma": "es5" }],
+      "react/no-string-refs": "off",
+      "react/no-unused-prop-types": "off",
+      "import/no-unresolved": 0,
+      "import/extensions": 0,
+      "import/no-extraneous-dependencies": "off",
+      "no-console": "off",
+      "prettier/prettier": [
+        "error",
+        {
+          "singleQuote": true,
+          "parser": "flow",
+          "trailingComma": "es5"
+        }
+      ],
+      "react/jsx-wrap-multilines": 0,
+      "arrow-parens": [
+        "error",
+        "as-needed"
+      ],
+      "react/jsx-filename-extension": [
+        1,
+        {
+          "extensions": [
+            ".js",
+            ".jsx"
+          ]
+        }
+      ]
+    },
+    "globals": {
+      "fetch": true
     }
+  }
 };
