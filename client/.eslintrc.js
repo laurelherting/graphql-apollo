832865ade7a409cf6d6bed5061318a9a8dde325d
Laurel Herting <laurel.herting@gmail.com>
2018-10-18 09:15:00 -0600

    remove eslintConfig for client

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,57 +1,53 @@
 module.exports = {
- "lint-staged": {
-    "*.js": [
-      "eslint --fix",
-      "git add"
+  "extends": [
+    "airbnb",
+    "prettier",
+    "prettier/react"
+  ],
+  "env": {
+    "browser": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 6
+  },
+  "plugins": [
+    "prettier"
+  ],
+  "extends": [
+    "airbnb-base",
+    "prettier"
+  ],
+  "rules": {
+    "react/no-string-refs": "off",
+    "react/no-unused-prop-types": "off",
+    "import/no-unresolved": 0,
+    "import/extensions": 0,
+    "import/no-extraneous-dependencies": "off",
+    "no-console": "off",
+    "prettier/prettier": [
+      "error",
+      {
+      "singleQuote": true,
+        "parser": "flow",
+        "trailingComma": "es5",
+      }
+    ],
+    "react/jsx-wrap-multilines": 0,
+    "arrow-parens": [
+      "error",
+      "as-needed"
+    ],
+    "react/jsx-filename-extension": [
+      1,
+      {
+        "extensions": [
+          ".js",
+          ".jsx"
+        ]
+      }
     ]
   },
-  "eslintConfig": {
-    "extends": [
-      "airbnb",
-      "prettier",
-      "prettier/react"
-    ],
-    "env": {
-      "browser": true
-    },
-    "parserOptions": {
-      "ecmaVersion": 6
-    },
-    "plugins": [
-      "prettier"
-    ],
-    "rules": {
-      "react/no-string-refs": "off",
-      "react/no-unused-prop-types": "off",
-      "import/no-unresolved": 0,
-      "import/extensions": 0,
-      "import/no-extraneous-dependencies": "off",
-      "no-console": "off",
-      "prettier/prettier": [
-        "error",
-        {
-          "singleQuote": true,
-          "parser": "flow",
-          "trailingComma": "es5"
-        }
-      ],
-      "react/jsx-wrap-multilines": 0,
-      "arrow-parens": [
-        "error",
-        "as-needed"
-      ],
-      "react/jsx-filename-extension": [
-        1,
-        {
-          "extensions": [
-            ".js",
-            ".jsx"
-          ]
-        }
-      ]
-    },
-    "globals": {
-      "fetch": true
-    }
+  "globals": {
+    "fetch": true
   }
 };
