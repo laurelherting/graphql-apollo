ce86191388ed3c5ea1fc9675be7804e7d9a0be10
Laurel Herting <laurel.herting@gmail.com>
2018-10-29 14:39:42 -0600

    wip-mutation

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -19,7 +19,7 @@ const getBooksQuery = gql`
 `;
 
 const addBookMutation = gql`
-  mutation($name: String!, $genre: String!, $authorId: ID!) {
+  mutation AddBook($name: String!, $genre: String!, $authorId: ID!) {
     addBook(name: $name, genre: $genre, authorId: $authorId) {
       name
       id
