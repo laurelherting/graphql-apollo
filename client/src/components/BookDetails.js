b08bfa4c7e133a6285855722b9308cee419eb678
Laurel Herting <laurel.herting@gmail.com>
2018-10-30 17:03:16 -0600

    wip-prettier fix

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -4,21 +4,21 @@ import { getBookQuery } from '../queries/queries';
 
 class BookDetails extends Component {
     displayBookDetails(){
-      const { book } = this.props.data;
+      const {book} = this.props.data;
       if (book){
-        return (
-          <div>
-            <h2>{ book.name }</h2>
-            <p>{ book.genre }</p>
-            <p>{ book.author.name }</p>
-            <p>All books by this author:</p>
-            <ul className="other-books">
-              { book.author.books.map(item => {
-                return <li key={item.id}>{ item.name }</li>
-              })}
-            </ul>
-          </div>
-        );
+      return (
+        <div>
+          <h2>{book.name}</h2>
+          <p>{book.genre}</p>
+          <p>{book.author.name}</p>
+          <p>All books by this author:</p>
+          <ul className="other-books">
+            {book.author.books.map(item => {
+            return <li key={item.id}>{item.name}</li>
+          })}
+        </ul>
+      </div>
+      );
       } else {
       return ( <div>No book selected...</div> );
       }
