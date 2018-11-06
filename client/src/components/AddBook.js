b2e12bbf20f1f0508e78ca0f0c80a9cab7d8a26d
Laurel Herting <laurel.herting@gmail.com>
2018-11-05 18:10:41 -0700

    import in books query

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { graphql, compose } from 'react-apollo';
-import { getAuthorsQuery, addBookMutation } from '../queries/queries';
+import { getAuthorsQuery, addBookMutation, getBooksQuery } from '../queries/queries';
 
 class AddBook extends Component {
   constructor(props) {
@@ -35,6 +35,7 @@ class AddBook extends Component {
           genre: this.state.genre,
           authorId: this.state.authorId
         },
+        refectchQueries: [{ query: getBooksQuery }]
       });
     }
 
