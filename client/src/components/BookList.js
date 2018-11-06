210abef320b177d7c0eac93854459dc9ac0c617c
Laurel Herting <laurel.herting@gmail.com>
2018-11-06 12:41:30 -0700

    import back in bookDetails

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import { getBooksQuery } from '../queries/queries';
+
+// components
+import BookDetails from './BookDetails';
 
 class BookList extends Component {
   render() {
@@ -7,9 +12,10 @@ class BookList extends Component {
         <ul id="book-list">
           <li>Book name</li>
         </ul>
+        <BookDetails />
       </div>
     );
   }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
